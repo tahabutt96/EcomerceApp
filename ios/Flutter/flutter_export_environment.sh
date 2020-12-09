d0464dbd79e5c87e5fa0a95f31193fb66d4ab5b7
tahabutt96 <tahabutt1996@gmail.com>
2020-12-09 20:57:48 +0500

    push code

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/muhammadanasshahid/Downloads/flutter-2"
+export "FLUTTER_APPLICATION_PATH=/Volumes/ENT/dd1"
+export "FLUTTER_TARGET=/Volumes/ENT/dd1/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/muhammadanasshahid/Downloads/flutter-2/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.7.0"
+export "FLUTTER_BUILD_NUMBER=1.7.0"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
